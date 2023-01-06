37c4ef1cf0db47874e8abd6e49ffd00ca8f3a760
Nursen Özcan <nursenozcan95@gmail.com>
2023-01-06 19:41:31 +0300

    Add project files.

---
+++ ConsoleApp6January/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace ConsoleApp6January
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("c:\\");
+             var dirList = di.GetDirectories();
+
+            foreach(var folder in dirList)
+            {
+                Console.WriteLine(folder.Name);
+            }
+
+        }
+        static void Dön (DirectoryInfo di)
+        {
+            var dirList = di.GetDirectories();
+            foreach (var folder in dirList)
+
+            {
+                Console.WriteLine (folder.Name);
+                Dön(folder);
+            }
+        }
+    }
+}
